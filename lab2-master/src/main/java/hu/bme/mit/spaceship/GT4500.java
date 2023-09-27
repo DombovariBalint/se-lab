a8b0666930578b9694f43df582616b04c4816f49
Dombovari Balint <balint2@icloud.com>
2023-09-27 15:28:52 +0200

    megre conflict

--- lab2-master/src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ lab2-master/src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,6 +80,7 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
         //Szerkesztés branch-A
+        //branch-B szerkesztése
         if(! secondaryTorpedoStore.isEmpty()){
           if(! primaryTorpedoStore.isEmpty()){
             firingSuccess = primaryTorpedoStore.fire(1);
