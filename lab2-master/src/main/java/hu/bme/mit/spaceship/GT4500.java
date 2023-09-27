6d0fb74f361eeb537b6b312aff3669ec8d3e41c3
Dombovari Balint <balint2@icloud.com>
2023-09-27 15:16:51 +0200

    branch-A szerkesztése

--- lab2-master/src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ lab2-master/src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,6 +79,7 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+        //Szerkesztés branch-A
         if(! secondaryTorpedoStore.isEmpty()){
           if(! primaryTorpedoStore.isEmpty()){
             firingSuccess = primaryTorpedoStore.fire(1);
